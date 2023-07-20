40f6c4467966b97e693f9da254298921d8280487
timel2ss <dyddn0823@hanyang.ac.kr>
2023-07-20 11:45:41 +0900

    Initial Commit

---
+++ src/main/java/com/petit/toon/ToonApplication.java
@@ -0,0 +1,13 @@
+package com.petit.toon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petit/toon/ToonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petit.toon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
