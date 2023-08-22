23a5f2b6f93d952c3d1f461866066efab155e7ca
timel2ss <dyddn0823@hanyang.ac.kr>
2023-08-23 01:41:03 +0900

    chore: 새로 저장된 이미지에 대한 외부 리소스 접근 설정 추가

--- src/main/java/com/petit/toon/config/WebConfig.java
+++ src/main/java/com/petit/toon/config/WebConfig.java
@@ -1,15 +1,29 @@
 package com.petit.toon.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.io.File;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${app.resource}")
+    private String path;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("file:" + File.separator + path);
     }
 }
